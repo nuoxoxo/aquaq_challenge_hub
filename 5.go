4bc6587b693ec55e8191a485ab7e03c640997d51
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-09-22 22:48:38 +0200

    5/10704

--- 5.go
+++ 5.go
@@ -6,15 +6,110 @@ import (
     "io/ioutil"
     "net/http"
     "strings"
-    _"strconv"
 )
+
+// object
+
+type Die struct {
+    faces map[string]int
+}
+
+func New_Die (initial_state map[string]int) *Die {
+    return & Die { faces: initial_state }
+}
+
+func (d *Die) Faces() map[string]int {
+    return d.faces
+}
+
+func (d *Die) Show (idx *int) {
+    if idx != nil {
+        fmt.Println(*idx, d.faces)
+    } else {
+        fmt.Println(d.faces)
+    }
+}
+
+func (d *Die) Roll(op string) {
+    if op == "L" { d.Roll_Left() }
+    if op == "R" { d.Roll_Right() }
+    if op == "U" { d.Roll_Up() }
+    if op == "D" { d.Roll_Down() }
+}
+
+func (d *Die) Roll_Up(){
+    up := d.faces["front"]
+    front := d.faces["down"]
+    d.faces["up"] = up
+    d.faces["down"] = 7 - up
+    d.faces["front"] = front
+    d.faces["back"] = 7 - front
+}
+
+
+func (d *Die) Roll_Down(){
+    up := d.faces["back"]
+    front := d.faces["up"]
+    d.faces["up"] = up
+    d.faces["down"] = 7 - up
+    d.faces["front"] = front
+    d.faces["back"] = 7 - front
+}
+
+func (d *Die) Roll_Left(){
+    left := d.faces["front"]
+    front := d.faces["right"]
+    d.faces["left"] = left
+    d.faces["right"] = 7 - left
+    d.faces["front"] = front
+    d.faces["back"] = 7 - front
+}
+
+func (d *Die) Roll_Right(){
+    left := d.faces["back"]
+    front := d.faces["left"]
+    d.faces["left"] = left
+    d.faces["right"] = 7 - left
+    d.faces["front"] = front
+    d.faces["back"] = 7 - front
+}
+
+// solve
 
 func main() {
 
     URL := "https://challenges.aquaq.co.uk/challenge/5/input.txt"
-    body := strings.TrimSpace(string(getbody(URL)))
-    fmt.Println("body/", body)
+    ops := strings.TrimSpace(string(getbody(URL)))
+    //ops = "LRDLU" // TEST
+    fmt.Println("body/", ops)//body)
 
+    da := New_Die(map[string]int{ "up": 3, "front": 1, "left": 2,
+        "down": 7 - 3, "back": 7 - 1, "right": 7 - 2,
+    })
+
+    db := New_Die(map[string]int{ "up": 2, "front": 1, "left": 3,
+        "down": 7 - 2, "back": 7 - 1, "right": 7 - 3,
+    })
+
+    da.Show(nil)
+    db.Show(nil)
+    res := 0
+    matches := []int{}
+    for i, op := range ops {
+        da.Roll(string(op))
+        db.Roll(string(op))
+        da.Show(&i)
+        db.Show(&i)
+        if da.Faces()["front"] == db.Faces()["front"] {
+            matches = append(matches, i)
+            res += i
+        }
+    }
+
+    da.Show(nil)
+    db.Show(nil)
+    fmt.Println("matches/", matches)
+    fmt.Println("res/", res)
 }
 
 func getbody(URL string) []uint8 {
