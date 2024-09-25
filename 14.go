f3f535d3f0c156ab742f72a59f843704ad0a3d86
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-09-25 23:14:23 +0200

    14/done

--- 14.go
+++ 14.go
@@ -18,44 +18,23 @@ var Coors map[int][2]int
 var numseq [][]int
 
 func main() {
-
-    for _, row := range G { fmt.Println(CYAN ("grid/"), len(row), row) }
-    for _, sq := range numseq {fmt.Println("nseq/" + Yell, sq[:12], Rest, len(sq))}
-    for k, v := range Coors { fmt.Println(k, v) }
-
-    // TEST
-    //numseq = [][]int{[]int{10,5,21,45,53,70,66,4},[]int{10,5,21,45,53,70,66,4},[]int{10,5,21,45,53,70,66,4},[]int{10,5,21,45,53,70,66,4}}
-
-    // bruteforce
     res := 0
-    for idx, sq := range numseq {
-        temp := len(sq)
-        // fmt.Println(sq, len(sq))
-        for left := 0; left < len(sq) - N + 1; left++ {
-            //var g [N][N][2]int
-            //g = G
-            g := G
-            for right := left; right < len(sq); right++ {
-                //fmt.Println(left, right)
-                // fmt.Printf("G/ %p - g/ %p \n", &G, &g) // proof of copying
-                key := sq[right]
-                if _, has := Coors[key]; !has { continue }
-                r, c := Coors[key][0], Coors[key][1]
-                g[r][c][1] = 1
-                dist := right - left + 1
-                //fmt.Println(CYAN("win/"), wins (g))
-                // if left == 0 && right == 6 { fmt.Println(g, key, Coors[key], r, c) }
-                if dist >= N && wins (g) && temp > dist {
-                    temp = dist
-                    fmt.Println(idx, "- winning sub/", sq[left : right + 1], "l - r/", left, right)
-                    for _, gg := range g {fmt.Println(gg)}
-                    break
-                }
+    for _, sq := range numseq {
+        g := G // this already deepcopies
+        for i := 0; i < len(sq); i++ {
+            key := sq[i]
+            if _, has := Coors[key]; !has {
+                continue
+            }
+            r, c := Coors[key][0], Coors[key][1]
+            g[r][c][1] = 1
+            if wins (g) {
+                res += i + 1
+                break
             }
         }
-        res += temp
     }
-    fmt.Println("res/", res)
+    fmt.Println("res/" + Yell, res, Rest)
 }
 
 func wins (g [N][N][2]int) bool {
@@ -88,15 +67,11 @@ func init() {
     re := regexp.MustCompile(`(?s)<pre>(.*?)</pre>`)
     lines := strings.Split(re.FindAllStringSubmatch(string(body), -1)[0][1], "<br>")[1:]
 
-    // DBG
-    // fmt.Println(CYAN ("lines/"), lines, len(lines))
 
     Coors = make(map[int][2]int)
     r := 0
     for _, line := range lines {
         subs := strings.Split(line, " ")
-        // DBG
-        fmt.Println(YELL ("subs/"), subs, len(subs))
         c := 0
         for _, sub := range subs {
             if sub == "" || sub == " " {
@@ -109,15 +84,9 @@ func init() {
         }
         r++
     }
-    // DBG
-    // for _, row := range G { fmt.Println(CYAN ("row/"), len(row), row) }
-
-    // Step - number sequences
 
     URL = "https://challenges.aquaq.co.uk/challenge/14/input.txt"
     lines = strings.Split(strings.TrimSpace(string(getbody(URL))), "\n")
-    // DBG
-    // for _, line := range lines {fmt.Println("line/", CYAN (line[:21]), "...")}
     for _, line := range lines {
         sub := strings.Split(line, " ")
         temp := []int{}
