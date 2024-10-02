f1c46815ba0ca29fbe7820cb100216bc9686cf57
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-10-02 22:45:00 +0200

    21/done

--- 21.go
+++ 21.go
@@ -9,47 +9,54 @@ import (
     "strconv"
 )
 
-type Tile struct { // ie. HeapNode
-    Gain    int
-    Coor    [2]int
-}
-
+var is_testing bool = false
+var window int = 3
 var tiles [][]int
 
 func main() {
-    fmt.Println(YELL("res/"), DP(tiles))
-}
-
-func DP (tiles [][]int) int {
-    //tiles=[][]int{{3, 4, 5, 1, 3},{9, 3, 4, 0, 9},{4, 5, 4, 4, 7},{3, 7, 9, 8, 2},{2,1,1,1,1}}
-    R, C := len(tiles), len(tiles[0]) - 5
-    dp := make([][]int, R)
-    for r := 0; r < R; r++ {
-        dp[r] = make([]int, C + 1)
-    }
-    // 0-th row
-    for c := 0; c <= C; c++ {
-        for sub := c; sub < c + 5; sub++ {
-            dp[0][c] += tiles[0][sub]
-        }
-    }
-    for r := 1; r < R; r++ {
-        for c := 0; c <= C; c++ {
-            curr := dp[r - 1][c]
-            if c > 0 { curr = max2(curr, dp[r - 1][c - 1]) }
-            if c < C { curr = max2(curr, dp[r - 1][c + 1]) }
-            todo := 0
-            for sub := c; sub < c + 5; sub++ {
-                todo += tiles[r][sub]
+    var DP func([][]int)int
+    DP = func (tiles [][]int)int{
+        if is_testing {
+            tiles = [][]int{
+                {3, 4, 5, 1, 3},
+                {9, 3, 4, 0, 9},
+                {4, 5, 4, 4, 7},
+                {3, 7, 9, 8, 2},
             }
-            dp[r][c] = curr + todo
+            window = 3
         }
+        R, C := len(tiles), len(tiles[0]) - window
+        dp := make([][]int, R)
+        for r := 0; r < R; r++ {
+            dp[r] = make([]int, C + 1)
+        }
+        // 0-th row
+        for c := 0; c <= C; c++ {
+            for sub := c; sub < c + window; sub++ {
+                dp[0][c] += tiles[0][sub]
+            }
+        }
+        if is_testing { fmt.Println(0, "-", dp[0]) }
+        for r := 1; r < R; r++ {
+            for c := 0; c <= C; c++ {
+                curr := dp[r - 1][c]
+                if c > 0 { curr = max2(curr, dp[r - 1][c - 1]) }
+                if c < C { curr = max2(curr, dp[r - 1][c + 1]) }
+                todo := 0
+                for sub := c; sub < c + window; sub++ {
+                    todo += tiles[r][sub]
+                }
+                dp[r][c] = curr + todo
+            }
+            if is_testing { fmt.Println(r, "-", dp[r]) }
+        }
+        res := 0
+        for c := 0; c <= C; c++ {
+            if res < dp[R - 1][c] { res = dp[R - 1][c] }
+        }
+        return res
     }
-    res := 0
-    for c := 0; c <= C; c++ {
-        if res < dp[R - 1][c] { res = dp[R - 1][c] }
-    }
-    return res
+    fmt.Println(YELL("res/"), DP(tiles))
 }
 
 func max2 (l, r int)int{
@@ -57,9 +64,7 @@ func max2 (l, r int)int{
     return r
 }
 
-func max3 (l, m, r int)int{
-    return max2(max2(l, m), r)
-}
+func max3 (l, m, r int)int{ return max2(max2(l, m), r) }
 
 func init() {
     // input
