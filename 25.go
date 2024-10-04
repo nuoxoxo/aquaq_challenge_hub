a8bb726f807636dc6537cbe3fc654b590cd20e81
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-10-04 09:35:15 +0200

    25/done - cleanup

--- 25.go
+++ 25.go
@@ -10,10 +10,26 @@ import (
     "time"
 )
 
+var morse map[string]string
+
 func main() {
 
-    URL := "https://challenges.aquaq.co.uk/challenge/25/input.txt"
-    line := strings.TrimSpace(string(getbody(URL)))
+    morse = make(map[string]string)
+    URL := "https://challenges.aquaq.co.uk/challenge/25"
+    re := regexp.MustCompile(`(?s)<pre>(.*?)</pre></div>`)
+    line := re.FindAllStringSubmatch( string(getbody(URL)), -1 )[0][1]
+    lines := strings.Split(strings.TrimSpace(line), "<br>")
+    for _, line := range lines {
+        pair := strings.Split(line, "| ")
+        morse[pair[1]] = pair[0]
+    }
+
+    // TEST
+    test_cipher := ".--- .- --   -.. --- -. ..- -"
+    fmt.Println("test/", YELL(dcode( test_cipher )))
+
+    URL = "https://challenges.aquaq.co.uk/challenge/25/input.txt"
+    line = strings.TrimSpace(string(getbody(URL)))
     timestamps := strings.Split(line, "\n")
     format := "15:04:05.000"
 
@@ -34,43 +50,52 @@ func main() {
         temp = append(temp, diff)
     }
 
-    URL = "https://challenges.aquaq.co.uk/challenge/25"
-    re := regexp.MustCompile(`(?s)<pre>(.*?)</pre></div>`)
-    line = re.FindAllStringSubmatch( string(getbody(URL)), -1 )[0][1]
-    lines := strings.Split(strings.TrimSpace(line), "<br>")
-    morse := make(map[string]string)
-    for _, line := range lines {
-        pair := strings.Split(line, "| ")
-        morse[pair[1]] = pair[0]
-    }
-    test_cipher := ".--- .- --   -.. --- -. ..- -"
-    fmt.Println("test/", YELL(dcode( morse, test_cipher )))
-
     // soln
     res := ""
+    cipher := ""
     for _, diff := range diffs {
         unit_size := diff[0]
-        morsechar := ""
-        for i := 1; i < len(diff); i++ { if unit_size > diff[i] { unit_size = diff[i] }}
+        running_char := ""
+        for i := 1; i < len(diff); i++ {
+            if unit_size > diff[i] { unit_size = diff[i] }
+        }
         for i, duration := range diff {
             size := int((duration / unit_size))// / time.Nanosecond)
             if i % 2 == 0 { // btw on and off
-                if size == 3 { morsechar += "-" }
-                if size == 1 { morsechar += "." }
+                if size == 3 { running_char += "-" }
+                if size == 1 { running_char += "." }
+                if size == 3 { cipher += "-" }
+                if size == 1 { cipher += "." }
             } else if size == 3 || size == 7 { // btw off and the next on
-                res += morse[morsechar]
-                morsechar = ""
+                res += morse[running_char]
+                running_char = ""
                 if size == 7 { res += " " }
+                cipher += " "
+                if size == 7 { cipher += "  " }
+
             }
         }
-        if morsechar != "" { res += morse[morsechar] }
+        if running_char != "" { res += morse[running_char] }
+        if running_char != "" { cipher += "   " }
         res += "\n"
         fmt.Println(CYAN("res/add"), res)
     }
     fmt.Println(YELL("res/"), res)
+    res2 := dcode( cipher )
+    fmt.Println(YELL("res/2"), res2)
+
+    // investigate the test line
+    slc := strings.Split(strings.TrimSpace(res2), " ")
+    ignored := slc[len(slc) - 1]
+    fmt.Println(YELL("ignored/"), ignored)
+    seen := make(map[string]bool)
+    for c := 'a'; c <= 'z'; c++ { seen[string(c)] = true }
+    for _, c := range ignored { seen[string(c)] = false }
+    for k, v := range seen { if v { fmt.Println(CYAN("letters left/"), k, v) } }
+    fmt.Println(YELL("(nothing found)"))
 }
 
-func dcode (morse map[string]string, cipher string) string {
+func dcode (/*morse map[string]string,*/ cipher string) string {
     res := ""
     slc := strings.Split(strings.Replace(cipher, "   ", "  ", -1), " ")
     for _, key := range slc {
@@ -80,7 +105,7 @@ func dcode (morse map[string]string, cipher string) string {
             res += morse[key]
         }
     }
-    return res
+    return res + "\n"
 }
 
 func getbody(URL string) []uint8 {
